fda01dec426f13d4e28d6bc31f94b22c48e67172
devMrkuzz <send2hire.mark@gmail.com>
2026-02-13 16:22:52 +0800

    User Interface changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { FontAwesome } from "@expo/vector-icons";
 import {
   Image,
   KeyboardAvoidingView,
@@ -14,8 +15,7 @@ export default function HomeScreen() {
   return (
     <KeyboardAvoidingView
       style={{ flex: 1, backgroundColor: "#800000" }}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20} // adjust for header if any
+      behavior={Platform.OS === "ios" ? "padding" : "padding"}
     >
       <ScrollView
         contentContainerStyle={styles.container}
@@ -34,6 +34,7 @@ export default function HomeScreen() {
         <View style={styles.fieldContainer}>
           <Text style={styles.header}>Sign Up to Continue</Text>
 
+          {/* USER ID */}
           <TextInput
             style={styles.input}
             placeholder="User ID"
@@ -41,37 +42,71 @@ export default function HomeScreen() {
             placeholderTextColor="#555"
           />
 
+          {/* PASSWORD */}
           <TextInput
             style={styles.input}
             placeholder="Password"
-            secureTextEntry={true}
+            secureTextEntry
             placeholderTextColor="#555"
           />
 
+          {/* FORGOT PASSWORD */}
           <Pressable
             onPress={() => console.log("Forgot Password pressed")}
             style={{ alignSelf: "flex-end", marginBottom: 20 }}
           >
-            <Text style={{ color: "#800000", textDecorationLine: "underline" }}>
+            <Text
+              style={{
+                color: "#800000",
+                textDecorationLine: "underline",
+                fontWeight: "500",
+              }}
+            >
               Forgot Password?
             </Text>
           </Pressable>
 
+          {/* SIGN UP BUTTON */}
           <Pressable
             style={styles.button}
             onPress={() => console.log("Sign Up pressed")}
           >
-            <Text style={styles.buttonText}>Sign Up</Text>
+            <Text style={styles.buttonText}>Continue</Text>
           </Pressable>
 
-          <Text style={styles.divider}>OR</Text>
+          {/* OR Divider */}
+          <Text style={styles.divider}>Sign in with</Text>
 
-          <Pressable
-            style={styles.googleBtn}
-            onPress={() => console.log("Sign Up with Google")}
-          >
-            <Text style={styles.googleTxt}>Sign up with Google</Text>
-          </Pressable>
+          {/* SIGN UP WITH Socials */}
+          <View style={styles.socials}>
+            <Pressable onPress={() => console.log("Google Pressed")}>
+              <FontAwesome name="google" size={30} color="#4285F4" />
+            </Pressable>
+
+            <View style={{ width: 20 }} />
+
+            <Pressable onPress={() => console.log("Facebook Pressed")}>
+              <FontAwesome name="facebook" size={30} color="#4285F4" />
+            </Pressable>
+          </View>
+
+          <View style={styles.create}>
+            <Text style={{}}>Don't have account?</Text>
+            <Pressable onPress={() => console.log("Create Account Clicked")}>
+              <Text
+                style={{ fontSize: 16, fontWeight: "bold", color: "#800000" }}
+              >
+                Sign Up
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+
+        {/* FOOTER */}
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>
+            © 2026 Your Company. All rights reserved.
+          </Text>
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
@@ -79,23 +114,6 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  googleBtn: {
-    padding: 10,
-    backgroundColor: "#4285F4",
-    borderRadius: 10,
-    width: "100%",
-    alignItems: "center",
-  },
-
-  googleTxt: {
-    color: "white",
-  },
-
-  divider: {
-    marginTop: 20,
-    color: "#424242ff",
-  },
-
   container: {
     flexGrow: 1,
     justifyContent: "center",
@@ -103,9 +121,10 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   logo: {
-    width: 150,
-    height: 150,
+    width: 100,
+    height: 100,
     marginBottom: 20,
+    resizeMode: "contain",
   },
   title: {
     fontSize: 30,
@@ -125,31 +144,62 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 5,
     elevation: 5,
+    marginBottom: 20,
   },
   header: {
     fontSize: 25,
     fontWeight: "bold",
     color: "#800000",
+    marginBottom: 15,
   },
   input: {
     borderWidth: 1,
     borderColor: "#800000",
-    borderRadius: 10,
-    padding: 12,
+    borderRadius: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
     width: "100%",
-    marginTop: 20,
-    marginBottom: 15,
+    marginVertical: 8,
     color: "black",
   },
   button: {
     backgroundColor: "#800000",
-    padding: 10,
-    borderRadius: 10,
+    padding: 15,
+    borderRadius: 20,
     width: "100%",
     alignItems: "center",
+    marginBottom: 15,
   },
   buttonText: {
     color: "white",
     fontSize: 16,
+    fontWeight: "600",
+  },
+  divider: {
+    color: "#555",
+    paddingVertical: 10,
+    fontSize: 14,
+  },
+  footer: {
+    marginTop: 10,
+    paddingVertical: 5,
+  },
+  footerText: {
+    color: "#eee",
+    fontSize: 12,
+  },
+
+  socials: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+    columnGap: 5,
+    marginBottom: 25,
+  },
+
+  create: {
+    flexDirection: "row",
+    columnGap: 5,
   },
 });
